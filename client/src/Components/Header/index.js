708d215e51f37e67dbf74bccae57887389ed4035
MinDuck <duccjhin@gmail.com>
2024-11-25 22:40:03 +0700

    edit env

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -121,10 +121,10 @@ const Header = () => {
         <div className="headerWrapper">
           <div className="top-strip bg-blue">
             <div className="container">
-              <p className="mb-0 mt-0 text-center">
+              {/* <p className="mb-0 mt-0 text-center">
                 Due to the <b>COVID 19</b> epidemic, orders may be processed
                 with a slight delay
-              </p>
+              </p> */}
             </div>
           </div>
 
